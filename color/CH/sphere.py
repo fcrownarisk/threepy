b940fd03052444bb0e39c5184da17b9311fe67c0
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-06-26 14:34:04 +0800

    Update sphere.py

--- color/CH/sphere.py
+++ color/CH/sphere.py
@@ -4,10 +4,10 @@ import color.Blue
 import color.anti
 import math
 def SpherePoint(t,theta,fai):
-   x = 'r' * 'sin'(theta) * 'cos'(fai)
-   y = 'r' * 'sin'(theta) * 'sin'(fai)
-   z = 'r' * 'cos'(fai)
-   T = 'r' * 'sin'(theta)
+   x = r * math.sin(theta) * math.cos(fai)
+   y = r * math.sin(theta) * math.sin(fai)
+   z = r * math.cos(fai)
+   T = r * math.sin(theta)
    return [x,y,z,T]
 def xyz():
     x = Red
@@ -17,13 +17,13 @@ def xyz():
     return [x,y,z,T]
 def SphereRadius(x,y,z):
    {SphereRadiusx,SphereRadiusy,SphereRadiusz}
-   SphereRadiusx = 'sin'(x) + 'cos'(y) + 'sin'('screenX') * 'cos'('screenY')
-   SphereRadiusy = 'sin'(y) + 'cos'(x) + 'sin'('screenY') * 'cos'('screenX')
-   SphereRadiusz = 'sin'(z) + 'cos'(y)
+   SphereRadiusx = math.sin(x) + math.cos(y) + math.sin(screenX) * math.cos(screenY)
+   SphereRadiusy = math.sin(y) + math.cos(x) + math.sin(screenY) * math.cos(screenX)
+   SphereRadiusz = math.sin(z) + math.cos(y)
    return [SphereRadiusx,SphereRadiusy,SphereRadiusz]
 def RGB():
-    thisx = Red
-    thisy = Green
-    thisz = Red
-    thist = anti
+    this.x = Red
+    this.y = Green
+    this.z = Red
+    this.t = anti
 print("x,y,z,t")
