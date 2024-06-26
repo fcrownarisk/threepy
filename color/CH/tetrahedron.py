79aad67f4f653a94841fc5f798cc47642cb6f0a1
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-06-26 14:34:38 +0800

    Update tetrahedron.py

--- color/CH/tetrahedron.py
+++ color/CH/tetrahedron.py
@@ -1,17 +1,17 @@
 import math
 def Cartesian(r, phi, theta):
   return [
-    r * 'cos'(phi) * 'sin'(theta),
-    r * 'sin'(phi) * 'sin'(theta),
-    r * 'cos'(theta)
+    r * math.cos(phi) * math.sin(theta),
+    r * math.sin(phi) * math.sin(theta),
+    r * math.cos(theta)
   ]
 def Cartesian():
   Length = 1; 
   r = Length
   vertices = [
-    'Red'(0,0,1),  
-    'Blue'('cos'(-1/3), 0, 0), 
-    'Green'(0, -'cos'(-1/3),0), 
-    'anti'(0,0,'cos'(-1/3)), 
+    Red(0,0,1),  
+    Blue(math.cos(-1/3), 0, 0), 
+    Green(0, -math.cos(-1/3),0), 
+    anti(0,0,math.cos(-1/3)), 
   ]
   
