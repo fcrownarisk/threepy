c8434aa016f35eeb622711957c853ddbd9111522
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-06-26 14:31:41 +0800

    Update methines.py

---
+++ color/CH/methines.py
@@ -0,0 +1,9 @@
+def CH2():
+     carbonCount  = 2;
+     hydrogenCount  = 4;
+def C6H8(): 
+     carbonCount  = 6;
+     hydrogenCount  = 8;
+def C14H16():
+     carbonCount  = 14;
+     hydrogenCount  = 16;
