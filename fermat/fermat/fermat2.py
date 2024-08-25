e79ed0addbdf935e3cb11b6d61d6bf170f25221c
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-08-24 20:43:56 -0700

    Update fermat2.py

--- fermat/fermat/fermat2.py
+++ fermat/fermat/fermat2.py
@@ -8,7 +8,5 @@ import xyz.smallxtensor
 import xyz.smallytensor
 import xyz.smallztensor
 def fermat2():
-    'BigXtensor' + 'midxtensor' + 'smallxtensor'=='BigYtensor'+'midytensor'+'smallytensor'=='BigZtensor'+'midztensor'+'smallztensor'
-    'BigXtensor - smallxtensor' + 'BigYtensor - smallytensor'+ 'BigZtensor - smallztensor'== 'midxtensor'+'midytensor'+'midztensor'
-    'BigXtensor * BigYtensor * BigZtensor'+'smallxtensor/smallytensor/smallztensor'=='0'
+    'BigXtensor' + 'midxtensor' + 'smallxtensor'==='BigYtensor'+'midytensor'+'smallytensor'==='BigZtensor'+'midztensor'+'smallztensor'
 print(fermat2)    
