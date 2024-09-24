f3d48b4f935d1682f295a8b5e3a01930e8f2f050
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-24 13:11:40 +0800

    Update fermat1.py

--- fermat/fermat/fermat1.py
+++ fermat/fermat/fermat1.py
@@ -2,5 +2,4 @@ import clock.Nulltensor
 import clock.onetensor
 import clock.ninetensor
 def fermat1():
-    Nulltensor ="onetensor + ninetensor"
-print(fermat1)    
+    Nulltensor ="onetensor + ninetensor"  
