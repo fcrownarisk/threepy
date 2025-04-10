8e5987a22a02d0888c6e82508949d7ef13bbc0b8
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-04-10 16:55:47 +0800

    Update RGBimpact.py

--- color/color/RGBimpact.py
+++ color/color/RGBimpact.py
@@ -2,13 +2,13 @@ import Red
 import Green
 import Blue
 import anti
-def impact(0,1,2,3,4):
+def impact():
    impact0 = 'element1 + element2 + element3 + element4'
    impact1 = 'element5 - element6 - element7 - element8'
    impact2 = 'element9 * element10 * element11 * element12'
    impact3 = 'element13 / element14 / element15 / element16'    
    impact4 = 'element17 % element18 % element19 % element20'
-def impact(5,6,7,8):  
+def impact():  
    impact5 = 'impact0 * impact1' + 'impact2 * impact3'
    impact6 = 'impact1 - impact2' + 'impact3 - impact4'
    impact7 = 'impact2 / impact3' + 'impact4 / impact0'
